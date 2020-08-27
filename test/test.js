abb39f24cf57961b66fd107dbd35a919353c8119
soumik <2000soumik@gmail.com>
2020-08-28 02:43:33 +0530

    third

--- test/test.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('.progress-bar').waypoint(function() {
-	$('.progress-bar').css({
-	animation: "animate-positive 2s",
-	opacity: "1"
-	});
-	}, { offset: '75%' 
-});

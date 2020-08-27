02add58264b3436df8f13d30bec2e46e5e75acc3
soumik <2000soumik@gmail.com>
2020-08-28 02:36:48 +0530

    second

--- js/mypage.js
+++ js/mypage.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    $('#header').fadeIn(1000).removeClass('hidden');
-    $('#skill').fadeIn(1500).removeClass('hidden');
-    $('#btn1').fadeIn(2000).removeClass('hidden');
-	
+    $('#header').fadeIn(1500).removeClass('hidden');
+    $('#skill').fadeIn(2000).removeClass('hidden');
+    $('#btnfirst').fadeIn(2000).removeClass('hidden');
+  
 });
 
 window.onscroll = function() {myFunction()};
@@ -14,37 +14,26 @@ function myFunction() {
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
   console.log(winScroll,"@",height)
-  if(winScroll>1850&&winScroll<2540&&flag==false){
-  	flag2=true;
-  	$("#htm").addClass("html");
-  	$("#cs").addClass("css");
-  	$("#java").addClass("java");
-  	$("#jqe").addClass("jquery");
+  if(winScroll>1030&&winScroll<2300&&flag==false){
+    flag2=true;
+    $("#htm").addClass("html");
+    $("#cs").addClass("css");
+    $("#java").addClass("java");
+    $("#jqe").addClass("jquery");
     $("#php").addClass("phP");
-  }else if(winScroll>2540 || winScroll<1050){
-  	
-  	flag2=false;
-  	$("#htm").removeClass("html");
-  	$("#cs").removeClass("css");
-  	$("#java").removeClass("java");
-  	$("#jqe").removeClass("jquery");
+    $("#djang").addClass("django");
+  }else if(winScroll>2400 || winScroll<700){
+    
+    flag2=false;
+    $("#htm").removeClass("html");
+    $("#cs").removeClass("css");
+    $("#java").removeClass("java");
+    $("#jqe").removeClass("jquery");
     $("#php").removeClass("phP");
+    $("#djang").removeClass("django");
   }
   
-  if(winScroll>2140&&winScroll<2540&&flag==false){
-  	flag=true;
-  	$("#workparent").addClass("animatedParent");
-  	if($("#workparent").addClass("animateOnce")){
-  		flag=false;
-  	}
-  	$("#workparent button").addClass("animated bounceInDown");
-
-  }else if (winScroll>2540 || winScroll<2140) {
-  	flag=false;
-  	$("#workparent").removeClass("animatedParent");
-  	$("#workparent button").removeClass("animated bounceInDown");
-
-  }
+  
 
 
 
